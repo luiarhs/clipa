4e59d8256ba5c26ec1a4af2f6fdfcf64057be118
Luis Angel <luiarhs@yandex.com>
2021-05-12 02:25:03 -0500

    [add] improves

--- src/main/java/mx/clip/api/controllers/HealthController.java
+++ src/main/java/mx/clip/api/controllers/HealthController.java
@@ -1,8 +1,11 @@
-package mx.clip.api.controller;
+package mx.clip.api.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +19,11 @@ public class HealthController {
     Logger logger = LoggerFactory.getLogger(HealthController.class);
 
     @GetMapping("/")
-    public String Get() {
-        return "API is healthy";
+    public ResponseEntity<Object> Get()
+    {
+        HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+
+        return new ResponseEntity<>(new Object[] { "API is healthy" }, headers, HttpStatus.OK);
     }
 }
